f6e9889659417effaf9ee253fda58baadec9fc30
Crazy_dreamer <38868670+crazy-dreamer1@users.noreply.github.com>
2022-02-08 20:36:06 +0600

    This is first commit. Mainly user creation, log in and multiplayer networking is changed here

---
+++ FORNITE/Scripts/GameAccount.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class GameAccount
+{
+    public string _id;
+    public int adminFlag;
+    public string username;
+}
---
+++ FORNITE/Scripts/LoginResponse.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class LoginResponse
+{
+    public int code;
+    public string msg;
+    public GameAccount data;
+}
